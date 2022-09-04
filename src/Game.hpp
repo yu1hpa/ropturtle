c5c5711089bbc456991accf92c15272c2a73d780
yu1hpa <yu.1hpa@gmail.com>
2022-09-05 02:45:51 +0900

    feat: Add Game core

--- src/Game.hpp
+++ src/Game.hpp
@@ -4,11 +4,19 @@
 class Game : public App::Scene
 {
 public:
-    
     Game(const InitData& init);
     ~Game();
     void update() override;
     void draw() const override;
+
+    void drawInstList() const;
+    void drawBlock() const;
+    void initStoredGadget();
 private:
-    Texture m_texture;
+    const Font font{ 20 };
+    std::vector<String> instList;
+    size_t chosenInst;
+    std::vector<String> storedGadget;
+    size_t chosenStoredIdx;
+    size_t ansInstNum;
 };
