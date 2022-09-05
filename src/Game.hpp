6550b557facbebf2110b78c8e04747bcb62558b0
yu1hpa <yu.1hpa@gmail.com>
2022-09-06 00:53:19 +0900

    feat: Add isCorrectAnswer func

--- src/Game.hpp
+++ src/Game.hpp
@@ -13,11 +13,13 @@ public:
     void drawBlock() const;
     void initStoredGadget();
     String pickAddress(size_t ci);
+    bool isCorrectAnswer();
 private:
     const Font font{ 20 };
     std::vector<String> instList;
     size_t chosenInst;
     std::vector<String> storedGadget;
     size_t chosenStoredIdx;
+    std::vector<String> answer;
     size_t ansInstNum;
 };
